01ebdf71de6fa7118f98de0d8eafa64752e0c275
AlexeyPogorelov <hirurg555@gmail.com>
2016-01-08 02:32:14 +0200

    mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,15 @@ $(document).on('ready', function () {
 		'min-height': winHeight
 	});
 
+	// open sidebar
+	$('.navbar-toggle').on('click', function () {
+		$($(this).data('target')).toggleClass('opened');
+	})
+	// open sidebar
+	$('.close-lightbox').on('click', function () {
+		$(this).closest('.mobile-menu').toggleClass('opened');
+	})
+
 	// resize
 	$(window).on('resize', function () {
 		winWidth = $(window).width();
